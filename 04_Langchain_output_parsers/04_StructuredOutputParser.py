c17e37f96d4a1739c9bb5971e72eb4a0328ee8a0
Sanika Mogal <sanikamogal925@gmail.com>
2026-02-26 09:27:08 +0530

    Output Parsers

--- 04_Langchain_output_parsers/04_StructuredOutputParser.py
+++ 04_Langchain_output_parsers/04_StructuredOutputParser.py
@@ -2,7 +2,29 @@ from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import StructuredOutputParser,ResponseSchema
 
 load_dotenv()
 
 model = ChatOllama(model="llama3")
+
+schema = [
+  ResponseSchema(name = 'fact_1',description = 'Fact 1 about the topic' ),
+  ResponseSchema(name = 'fact_2',description = 'Fact 2 about the topic' ),
+  ResponseSchema(name = 'fact_3',description = 'Fact 3 about the topic' ),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+  template='Give 3 facts about the {topic} \n  {format_instruction}',
+  input_variables=['topic'],
+  partial_variables={'format_instruction':parser.get_format_instruction()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
+
